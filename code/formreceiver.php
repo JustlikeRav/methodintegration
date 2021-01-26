5fa812af90dec2815c1fef2d882839ea93be5ccf
DESKTOP-8A2NBQ9\Rav <ravneetsngh6@gmail.com>
2021-01-26 17:28:09 -0500

    almost done

--- code/formreceiver.php
+++ code/formreceiver.php
@@ -1,19 +1,27 @@
-<table>
 <?php 
 
-$dataTable = "";
+$dataTable = "<table>";
     foreach ($_POST as $key => $value) {
-        $dataTable = "<tr>".$dataTable;
-        $dataTable = "<td>".$dataTable;
-        $dataTable = $key.$dataTable;
-        $dataTable = "</td>".$dataTable;
-        $dataTable = "<td>".$dataTable;
-        $dataTable = $value.$dataTable;
-        $dataTable = "</td>".$dataTable;
-        $dataTable = "</tr>".$dataTable;
+        $dataTable = $dataTable."<tr>";
+        $dataTable = $dataTable."<td>";
+        $dataTable = $dataTable.$key;
+        $dataTable = $dataTable."</td>";
+        $dataTable = $dataTable."<td>";
+        $dataTable = $dataTable.$value;
+        $dataTable = $dataTable."</td>";
+        $dataTable = $dataTable."</tr>";
     }
 
-echo $dataTable;
+$dataTable = $dataTable."</table>";
+
+$to      = 'ravneet@avenview.com';
+$subject = 'Form Submission '.$_POST['formName'];
+$message = $dataTable;
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: no-reply@avenview.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
 
 ?>
-</table>
